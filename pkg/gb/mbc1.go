1ee61834875f502df82abc7d853bd38a7cae14dc
Clarke <z5421183@ad.unsw.edu.au>
2024-06-03 03:13:42 +1000

    refactor(mbc1) && bug(mbc1): fixed bugs in mbc1 not working properly for certain games

--- pkg/gb/mbc1.go
+++ pkg/gb/mbc1.go
@@ -13,11 +13,11 @@ type MBC1 struct {
 	cart         *Cart
 	ram          []byte
 	ramEnabled   bool
-	romBankNum   uint8
 	romBankMask  uint8
 	romBankNBits uint8
-	ramBankNum   uint8
 	numBanks     uint8
+	ramBankNum   uint8
+	romLo        uint8
 	mode         BankMode
 }
 
@@ -30,8 +30,7 @@ const (
 
 func (mbc *MBC1) init(cart *Cart) {
 	mbc.cart = cart
-	mbc.romBankNum = 1
-	mbc.ramEnabled = false
+	mbc.romLo = 1
 	mbc.mode = MODE0
 	mbc.numBanks = uint8(cart.romSize / 0x4000)
 	mbc.romBankNBits = uint8(math.Log2(float64(mbc.numBanks)))
@@ -56,29 +55,28 @@ func (mbc *MBC1) read(addr uint16) uint8 {
 	if inRange(addr, ROM_BASE, ROM_TOP) {
 		if inRange(addr, ROM_BASE, 0x3FFF) {
 			// bank 00 of rom
+			if mbc.bigROM() && mbc.mode == MODE1 {
+				return mbc.cart.rom[(uint32((mbc.ramBankNum<<5)&mbc.romBankMask)*0x4000)+uint32(addr)]
+			}
+
 			return mbc.cart.rom[addr]
 		}
 
 		if inRange(addr, 0x4000, ROM_TOP) {
 			// switchable bank of rom
-			bank := mbc.romBankNum
-			// if mbc.mode == MODE1 {
-			// 	bank &= 0x1F
-			// }
-
-			return mbc.cart.rom[(uint16(bank)*0x4000)+addr-0x4000]
+			return mbc.cart.rom[(uint32(((mbc.ramBankNum<<5)|mbc.romLo)&mbc.romBankMask)*0x4000)+uint32(addr-0x4000)]
 		}
 	} else if inRange(addr, EXT_RAM_BASE, EXT_RAM_TOP) {
 		if !mbc.ramEnabled {
 			return 0xFF
 		}
 
-		bank := mbc.ramBankNum
-		// if mbc.mode == MODE0 {
-		// 	bank = 0
-		// }
+		bank := uint8(0)
+		if mbc.bigRam() && mbc.mode == MODE1 {
+			bank = mbc.ramBankNum
+		}
 
-		return mbc.ram[(uint16(bank)*0x2000)+addr-EXT_RAM_BASE]
+		return mbc.ram[(uint32(bank)*0x2000+uint32(addr-EXT_RAM_BASE))%mbc.cart.ramSize]
 	}
 
 	log.Fatalf("MMU mapped an illegal read address: 0x%02x to MBC1", addr)
@@ -95,57 +93,24 @@ func (mbc *MBC1) write(addr uint16, data uint8) {
 
 		if inRange(addr, 0x2000, 0x3FFF) {
 			// set lower 5 bits of rom bank register
-			if data == 0 {
-				// bank 0x00 -> 0x01 translation considers the 5 bits
-				data = 1
-			}
-
 			val := data & 0x1F
-
-			if mbc.romBankNBits > 5 {
-				mbc.romBankNum = (mbc.romBankNum & 0x60) | (val & mbc.romBankMask)
-			} else {
-				mbc.romBankNum = (val & mbc.romBankMask)
+			if val == 0 {
+				val = 1
 			}
+
+			mbc.romLo = val & mbc.romBankMask
 			return
 		}
 
 		if inRange(addr, 0x4000, 0x5FFF) {
 			// set either the upper 2 bits of rom bank register or select ram banks 0-3
-			val := data & 0x3
-
-			if mbc.mode == MODE0 {
-				if mbc.cart.romSize >= 0x100000 {
-					// only for 1 MiB or larger ROM carts
-					mbc.romBankNum = (mbc.romBankNum & 0x1F) | (val << 5)
-				}
-			} else {
-				if mbc.cart.ramSize == 0x8000 {
-					// only for 32 Kib RAM carts
-					mbc.ramBankNum = val
-				}
-			}
-
+			mbc.ramBankNum = data & 0x3
 			return
 		}
 
 		if inRange(addr, 0x6000, 0x7FFF) {
 			// set the banking mode
-			if mbc.cart.ramSize <= 0x2000 && mbc.cart.romSize <= 0x80000 {
-				return
-			}
-
-			val := data & 0x1
-			mbc.mode = BankMode(val)
-
-			if mbc.mode == MODE0 {
-				// only ram bank 0 can be accessed
-				mbc.ramBankNum = 0
-			} else {
-				// clear upper 2 bits
-				mbc.romBankNum &= 0x1F
-			}
-
+			mbc.mode = BankMode(data & 0x1)
 			return
 		}
 	} else if inRange(addr, EXT_RAM_BASE, EXT_RAM_TOP) {
@@ -153,9 +118,24 @@ func (mbc *MBC1) write(addr uint16, data uint8) {
 			return
 		}
 
-		mbc.ram[(uint16(mbc.ramBankNum)*0x2000)+addr-EXT_RAM_BASE] = data
+		bank := uint8(0)
+		if mbc.bigRam() && mbc.mode == MODE1 {
+			bank = mbc.ramBankNum
+		}
+
+		mbc.ram[(uint32(bank)*0x2000+uint32(addr-EXT_RAM_BASE))%mbc.cart.ramSize] = data
 		return
 	}
 
 	log.Fatalf("MMU mapped an illegal write address: 0x%02x to MBC1", addr)
+}
+
+func (mbc *MBC1) bigROM() bool {
+	// ROM can make use of the 2-bit register
+	return mbc.cart.romSize >= 0x100000
+}
+
+func (mbc *MBC1) bigRam() bool {
+	// RAM can make use of the 2-bit register
+	return mbc.cart.ramSize > 0x2000
 }
