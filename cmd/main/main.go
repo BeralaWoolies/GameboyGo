3096e12dbde5d15e32947d44432d1a416c6a7b6d
Clarke <z5421183@ad.unsw.edu.au>
2024-06-05 01:38:36 +1000

    refactor(main): removed dead arguments code

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,18 +2,11 @@ package main
 
 import (
 	"flag"
-	"log"
-	"os"
 
 	"github.com/BeralaWoolies/GameboyGo/pkg/gb"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 0 {
-		log.Fatalf("Usage: go run %s myrom.gb", os.Args[0])
-	}
-
 	rom := flag.String("rom", "", "specify a .gb rom")
 	debugMode := flag.Bool("d", false, "specify -d to enable debug mode")
 	flag.Parse()
