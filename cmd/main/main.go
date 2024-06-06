ee3c55777c5cf3345fcb75621596e53c01bea4f0
Clarke <z5421183@ad.unsw.edu.au>
2024-06-06 22:09:43 +1000

    testing(cpu and memory profiling): implemented cpu and memory profiling for emulator

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,14 +2,45 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/BeralaWoolies/GameboyGo/pkg/gb"
 )
 
+var cpuprofile *string = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile *string = flag.String("memprofile", "", "write memory profile to `file`")
+var rom *string = flag.String("rom", "", "specify a .gb rom")
+var debugMode *bool = flag.Bool("d", false, "specify -d to enable debug mode")
+
 func main() {
-	rom := flag.String("rom", "", "specify a .gb rom")
-	debugMode := flag.Bool("d", false, "specify -d to enable debug mode")
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC()
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 
 	gameboy := gb.NewGameboy(*rom, *debugMode)
 	gameboy.Start()
